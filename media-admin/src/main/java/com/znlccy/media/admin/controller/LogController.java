62f0bc8554a9595ed8cfdacd358d91dc19c38d44
chencongye <chen.congye@trs.com.cn>
2019-03-21 12:02:53 +0800

    添加日志和订单控制器， 部分js文件

---
+++ media-admin/src/main/java/com/znlccy/media/admin/controller/LogController.java
@@ -0,0 +1,21 @@
+package com.znlccy.media.admin.controller;
+
+import com.znlccy.media.core.controller.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * The type LogController
+ *
+ * FileName：LogController.java
+ * Description：	日志控制器
+ *
+ * History：
+ * 版本号 			作者 			日期       				简介
+ * 	1.0				chencongye		2019/3/21-10:43			create
+ */
+@Controller
+@RequestMapping(value = "/log")
+public class LogController extends BaseController {
+
+}
---
+++ media-admin/src/main/java/com/znlccy/media/admin/controller/OrderController.java
@@ -0,0 +1,21 @@
+package com.znlccy.media.admin.controller;
+
+import com.znlccy.media.core.controller.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * The type OrderController
+ *
+ * FileName：OrderController.java
+ * Description：	订单控制器
+ *
+ * History：
+ * 版本号 			作者 			日期       				简介
+ * 	1.0				chencongye		2019/3/21-10:41			create
+ */
+@Controller
+@RequestMapping(value = "/order")
+public class OrderController extends BaseController {
+
+}
